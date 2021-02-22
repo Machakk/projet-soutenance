c0b15c1b645185964e68088cb329dfb7e098a010
sawsanalkebsi <sawsan.alkebsi.com>
2021-02-22 21:36:13 +0100

    crud create user

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Users;
+use App\Form\UserType;
 use App\Repository\UsersRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UsersController extends AbstractController
 {
@@ -17,6 +21,34 @@ class UsersController extends AbstractController
         $users = $usersRepository->findAll();
         return $this->render('admin/users.html.twig', [
             'users' => $users,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/users/create", name="user_create")
+     */
+    public function createUser(Request $request, UserPasswordEncoderInterface $passwordEncoder ){
+        $user = new Users();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($user);
+            $manager->flush();
+            return $this->redirectToRoute('admin_users'); 
+        }
+
+        return $this->render('admin/userForm.html.twig', [
+            'userForm'=> $form->createView(),
         ]);
     }
 }
