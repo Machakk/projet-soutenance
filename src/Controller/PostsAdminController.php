408ac060701e618f670867cf02cb9113ff23b47d
sawsanalkebsi <sawsan.alkebsi.com>
2021-02-23 20:14:20 +0100

    crud ajoute +update+delete post espace admin

---
+++ src/Controller/PostsAdminController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\PostForum;
+use App\Form\PostsType;
+use App\Repository\PostForumRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PostsAdminController extends AbstractController
+{
+    /**
+     * @Route("/admin/posts", name="admin_posts")
+     */
+    public function index(PostForumRepository $postForumRepository): Response
+    {
+        $posts = $postForumRepository->findAll();
+        return $this->render('admin/posts.html.twig', [
+            'posts' => $posts,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/posts/create", name="post_create")
+     */
+    public function createPost(Request $request){
+
+        $post = new PostForum();
+        $form = $this->createForm(PostsType::class, $post);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+
+            $infoImg = $form['img']->getData();
+            if($infoImg !=null){
+
+                $extebsionImg = $infoImg->guessExtension();
+                $nomImg = '1-'. time() .'.'. $extebsionImg;// compose un nom d'image unique
+                $infoImg->move($this->getParameter('photos_posts') ,$nomImg); //déplace l'image dans le dossier
+     
+                $post->setImg($nomImg);
+            }
+
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($post);
+            $manager->flush();
+            return $this->redirectToRoute('admin_posts');
+            //métier modifié
+        }
+        return $this->render('admin/postsForm.html.twig', [
+            'postsForm'=>$form->createView(),
+        ]);
+
+    
+    }
+
+    /**
+     * @Route("/admin/posts/update-{id}", name="post_update")
+     */
+    public function updateSkills(PostForumRepository $postForumRepository, $id, Request $request)
+    {
+        $post = $postForumRepository->find($id);
+        $form = $this->createForm(PostsType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $oldNomImg1 = $post->getImg();
+            if($oldNomImg1 !=null){
+
+                $oldCheminImg1 = $this->getParameter('photos_posts') . '/' . $oldNomImg1;       
+                if (file_exists($oldCheminImg1)) 
+                {
+                    unlink($oldCheminImg1);
+                }
+            }
+           
+            $infoImg1 = $form['img']->getData();
+            if($infoImg1 !=null){
+                
+                $extensionImg1 = $infoImg1->guessExtension();
+                $nomImg1 = '1-' . time() . '.' . $extensionImg1;
+                $infoImg1->move($this->getParameter('photos_posts'), $nomImg1);
+                $post->setImg($nomImg1);
+            }
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($post);
+            $manager->flush();
+            $this->addFlash('success', 'Le post a été modifié');
+            return $this->redirectToRoute('admin_posts');
+        }
+        return $this->render('admin/postsForm.html.twig', [
+            'postsForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/posts/delete-{id}", name="post_delete")
+     */
+    public function deleteSkills(PostForumRepository $postForumRepository, $id) {
+        $post = $postForumRepository->find($id);
+        $oldNomImg1 = $post->getImg();
+        $oldCheminImg1 = $this->getParameter('photos_posts') . '/' . $oldNomImg1;
+        if ($oldNomImg1 != null) {
+            unlink($oldCheminImg1);
+        }
+        $manager = $this->getDoctrine()->getManager();
+        $manager->remove($post);
+        $manager->flush();
+     
+        return $this->redirectToRoute('admin_posts');
+    }
+}
---
+++ src/Form/PostsType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Users;
+use App\Entity\Metiers;
+use App\Entity\PostForum;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
+class PostsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('content' , TextType::class)
+            ->add('img' , FileType::class , [
+                'required' => false,
+            ])
+            ->add('date',DateTimeType::class)
+
+            ->add('metier', EntityType::class, [
+                'required' => true,
+                'class' => Metiers::class,
+                'choice_label' => 'metier',
+                
+            ])
+
+            ->add('user', EntityType::class, [
+                'required' => true,
+                'class' => Users::class,
+                'choice_label' => 'pseudo',
+                
+            ])
+
+            ->add('valider',SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => PostForum::class,
+        ]);
+    }
+}
