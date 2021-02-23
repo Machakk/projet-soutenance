df1d73264623ebe2536135633cb76dd7f1ccd795
sawsanalkebsi <sawsan.alkebsi.com>
2021-02-23 22:21:54 +0100

    crud create commentaire admin

--- src/Controller/CommentairesController.php
+++ src/Controller/CommentairesController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\CommentaireForum;
+use App\Form\CommentairesPostType;
+use Symfony\Component\HttpFoundation\Request;
 use App\Repository\CommentaireForumRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CommentairesController extends AbstractController
 {
@@ -17,6 +20,27 @@ class CommentairesController extends AbstractController
         $commentaires = $commentaireForumRepository->findAll();
         return $this->render('admin/commentaires.html.twig', [
             'commentaires' => $commentaires,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/commentaires/create", name="commentaire_create")
+     */
+    public function createPost(Request $request){
+        $commentaire = new CommentaireForum();
+        $form = $this->createForm(CommentairesPostType::class, $commentaire);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($commentaire);
+            $manager->flush();
+            return $this->redirectToRoute('admin_commentaires');
+        }
+
+        return $this->render('admin/commentaireForm.html.twig', [
+            'commentaireForm'=>$form->createView(),
         ]);
     }
 }
