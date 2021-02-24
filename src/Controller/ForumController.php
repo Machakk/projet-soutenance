f726dcc50a1beae2dd718a8409f39021a171fdc2
sawsanalkebsi <sawsan.alkebsi.com>
2021-02-24 13:50:42 +0100

    page forum- manque affiche user-photo + lire suite + commentaire

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PostForumRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,11 @@ class ForumController extends AbstractController
     /**
      * @Route("/forum", name="forum")
      */
-    public function index(): Response
+    public function index(PostForumRepository $postForumRepository): Response
     {
+        $posts = $postForumRepository->findAll();
         return $this->render('forum/forum.html.twig', [
-            'controller_name' => 'ForumController',
+            'posts' => $posts,
         ]);
     }
 }
