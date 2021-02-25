7c92592c58e27cdd6e1a4ca739dc62b721b41756
Milan Jovanovic <45638150+Madjunski@users.noreply.github.com>
2021-02-25 14:09:17 +0100

    post redirect à treminer

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -99,6 +99,7 @@ class AppAuthenticator extends AbstractFormLoginAuthenticator implements Passwor
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
         return new RedirectResponse($this->urlGenerator->generate('home'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+
     }
 
     protected function getLoginUrl()
