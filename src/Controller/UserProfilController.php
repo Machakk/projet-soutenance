646a8cc39becddaaae1fb7d6f88a880b2a36505e
sawsanalkebsi <sawsan.alkebsi.com>
2021-02-25 02:25:45 +0100

    affiche user profil + ses posts dans profil

--- src/Controller/UserProfilController.php
+++ src/Controller/UserProfilController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\PostForumRepository;
+use App\Repository\UsersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,15 +11,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class UserProfilController extends AbstractController
 {
     /**
-     * @Route("/profil", name="user_profil")
+     * @Route("/profil-{id}", name="user_profil")
      */
-    public function index(): Response
+    public function index(UsersRepository $usersRepository, $id, PostForumRepository $postForumRepository): Response
     {
+
+        $user = $usersRepository->find($id);
+        // $posts = $user->getPostForums();
+        $posts = $postForumRepository->findAll();
+        
         $icon = $this->getParameter('photos_icon');
         $iconFb = $icon . "/fb.png";
+        // var_dump($posts);
+        // die();
 
+        // $id = 
         return $this->render('user_profil/profil.html.twig', [
-            'controller_name' => 'UserProfilController',
+            'user' => $user,
+            'posts' => $posts
         ]);
 
     }
