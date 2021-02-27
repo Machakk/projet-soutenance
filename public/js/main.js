3a592fe69441bb2ed693660384c65bc9af6fbd55
Pakham <contact@pakham.fr>
2021-02-27 20:35:31 +0100

    création de faq + integration + responsive

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,19 @@
 function changeStyle(nouveauDisplay) {
     document.getElementById("login-post-all").style.display = nouveauDisplay;
   }
+function changeStyle2(newDisplay) {
+document.getElementById("answer2").style.display = newDisplay;
+}
+function changeStyle3(newDisplay) {
+document.getElementById("answer3").style.display = newDisplay;
+}
+function changeStyle4(newDisplay) {
+document.getElementById("answer4").style.display = newDisplay;
+}
+function changeStyle5(newDisplay) {
+document.getElementById("answer5").style.display = newDisplay;
+}
+function changeStyle6(newDisplay) {
+document.getElementById("answer6").style.display = newDisplay;
+}
+  
