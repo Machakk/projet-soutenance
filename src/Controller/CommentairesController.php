3e93ae7611997045b0b5a5faa4eada730870962a
Milan Jovanovic <45638150+Madjunski@users.noreply.github.com>
2021-02-28 00:31:48 +0100

    p

--- src/Controller/CommentairesController.php
+++ src/Controller/CommentairesController.php
@@ -143,8 +143,8 @@ class CommentairesController extends AbstractController
             $manager = $this->getDoctrine()->getManager();
             $manager->persist($commentaire);
             $manager->flush();
-
             $this->addFlash('success', 'Vous avez commenté ce post!');
+            return $this->redirectToRoute('commentaire_user_create', array('id' => $id));
         }
         $commentaires = $commentaireForumRepository->findAll();
         
@@ -154,86 +154,6 @@ class CommentairesController extends AbstractController
             'commentaireUserCreate'=>$form->createView(),
             'post'=> $post,
             'commentaires' => $commentaires
-            ]);
-        
+            ]);        
     }
-
-    // /**
-    //  * @Route("/forum/showpost-{id}", name="showpost")
-    //  */
-    // public function showPost(Request $request, PostForumRepository $postForumRepository, CommentaireForumRepository $commentaireForumRepository, $id)
-    // {
-
-    //     $user = $this->getUser();
-    //     $commentaire = new CommentaireForum();
-    //     $form = $this->createForm(CommentairePostUserType::class, $commentaire);
-    //     $form->handleRequest($request);
-    //     $post = $postForumRepository->find($id);
-        
-    //     if($form->isSubmitted() && $form->isValid())
-    //     {
-    //         $commentaire->setUser($user);
-    //         $commentaire->setPost($post);
-    //         $date = new \DateTime('@'.strtotime('now'));
-    //         $commentaire->setDate($date);
-    //         $manager = $this->getDoctrine()->getManager();
-    //         $manager->persist($commentaire);
-    //         $manager->flush();
-    //         $this->addFlash('success', 'Vous avez commenté ce post!');
-    //     }
-    //     $commentaires = $commentaireForumRepository->findAll();
-        
-        
-    //     return $this->render('forum/post.html.twig', [
-    //         'id' => $id,
-    //         'commentaireUserCreate'=>$form->createView(),
-    //         'post'=> $post,
-    //         'commentaires' => $commentaires
-    //         ]);
-    // }
-
-    
-
-    /* test 3 */
-
-    // /**
-    //  * @Route("/forum/showpost", name="showpost")
-    //  */
-    // public function showPost(Request $request, PostForumRepository $postForumRepository, $id)
-    // {
-
-    //     $user = $this->getUser();
-    //     $commentaire = new CommentaireForum();
-    //     $form = $this->createForm(CommentairePostUserType::class, $commentaire);
-    //     $form->handleRequest($request);
-    //     $post = $postForumRepository->find($id);
-
-    //     $posts = $postForumRepository->find($id);
-    //     $form2 = $this->createForm(PostsType::class, $posts);
-    //     $form2->handleRequest($request);
-
-    //     if($form->isSubmitted() && $form->isValid() && $form2->isSubmitted() && $form2->isValid())
-    //     {
-    //         $commentaire->setUser($user);
-    //         $commentaire->setPost($post);
-
-    //         $date = new \DateTime('@'.strtotime('now'));
-    //         $commentaire->setDate($date);
-    //         $manager = $this->getDoctrine()->getManager();
-    //         $manager->persist($commentaire);
-    //         $manager->flush();
-
-            
-    //         $manager2 = $this->getDoctrine()->getManager();
-    //         $manager2->persist($posts);
-    //         $manager2->flush();
-
-    //         return $this->redirectToRoute('commentaire_user_create', ['id' => $id]);
-    //     }
-
-    //     return $this->render('forum/post.html.twig', [
-    //         'commentaireUserCreate'=>$form->createView(),
-    //         'posts' => $posts,
-    //     ]);
-    // }
 }
